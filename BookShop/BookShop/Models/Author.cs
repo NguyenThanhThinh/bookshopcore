f95b02d63ef6025ff6f3ba49d1c9e3b79c435ed5
Thinh Nguyen <ntthinh@cusc.ctu.edu.vn>
2020-04-28 15:39:58 +0900

    add models

---
+++ BookShop/BookShop/Models/Author.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BookShop.Models
+{
+	public class Author
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public DateTime CreateDate { get; set; }
+
+		public List<Book> Books = new List<Book>();
+	}
+}
---
+++ BookShop/BookShop/Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BookShop.Models
+{
+	public class Category
+	{
+		public int Id { get; set; }
+
+		public string Name { get; set; }
+
+		public string Description { get; set; }
+
+		public DateTime CreateDate { get; set; }
+
+		public List<Book> Books = new List<Book>();
+	}
+}
